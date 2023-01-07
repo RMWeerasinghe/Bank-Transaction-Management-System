2531a9d15d3f5f4e7127e76f684aeb8d24654275
Gayani2001 <120321754+Gayani2001@users.noreply.github.com>
2023-01-07 18:56:41 +0530

    Create individual_customer.controller.js

---
+++ backend/app/controllers/individual_customer.controller.js
@@ -0,0 +1,73 @@
+const Individual_customer= require("../models/individual_customer.model.js");
+
+class Individual_customerController {
+
+    static async getAllIndividual_customers(req, res) {
+
+        Individual_customer.getAllIndividual_customers((err, result) => {
+            if (err) {
+                return res.status(500)
+                    .send({ error: "Something went wrong" })
+            }
+
+            return res.send(result)
+        })
+    }
+
+    static async getDetailsByCode(req, res) {
+
+        const customer_id = req.params.code
+
+        Individual_customer.getDetailsByCode(customer_id, (err, result) => {
+            if (err) {
+                return res.status(500)
+                    .send({ error: "Something went wrong." })
+            }
+
+            if (result.length === 0) {
+                return res.status(404)
+                    .send({ error: "Incorrect Customer ID" })
+            }
+
+            return res.send(result[0])
+        })
+    }
+
+    static async createNewIndividual_customer(req, res) {
+
+        const { customer_id,NIC,first_name,middle_name,last_name,date_of_birth,gender,nationality } = req.body
+
+        const new_individual_customer = new Individual_customer(customer_id,NIC,first_name,middle_name,last_name,date_of_birth,gender,nationality)
+
+        new_individual_customer.createNewIndividual_customer((err, result) => {
+
+            if (err) {
+                return res.status(500)
+                    .send({ error: "Something went wrong." })
+            }
+
+            return res.send(result)
+        })
+    }
+
+    static async deleteByCode(req, res) {
+
+        const customer_id = req.params.code
+
+        Individual_customer.deleteByCode(customer_id, (err, result) => {
+            if (err) {
+                return res.status(500)
+                    .send({ error: "Something went wrong." })
+            }
+
+            if (result.length === 0) {
+                return res.status(404)
+                    .send({ error: "Incorrect Customer ID" })
+            }
+
+            return res.send(result)
+        })
+    }
+
+}
+module.exports= Individual_customerController;
