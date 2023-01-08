83a5b57212c19ba72bc401c08e8ac43348c52311
HMMBaqir <99200277+HMMBaqir@users.noreply.github.com>
2023-01-08 13:08:10 +0530

    Add files via upload

---
+++ backend/app/routes/atm_transaction.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ATMTransactionController = require("./controllers/atm_transaction.controller.js");
+
+const router = express.Router();
+
+router.route("/")
+  .get(ATMTransactionController.getAllTransactions)
+  .post(ATMTransactionController.createNewTransaction);
+
+router.route("/:id")
+  .get(ATMTransactionController.getDetailsById)
+  .delete(ATMTransactionController.deleteById);
+
+module.exports = router;
---
+++ backend/app/routes/branch_manager.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const BranchManagerController = require("./controllers/branch_manager.controller.js");
+
+const router = express.Router();
+
+router.route("/")
+  .get(BranchManagerController.getAllManagers)
+  .post(BranchManagerController.createNewManager);
+
+router.route("/:branch_code")
+  .get(BranchManagerController.getDetailsByBranchCode)
+  .delete(BranchManagerController.deleteByBranchCode);
+
+module.exports = router;
