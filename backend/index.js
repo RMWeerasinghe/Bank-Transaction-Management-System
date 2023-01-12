97320731851ebb8af04bd350f654669cff374585
RMWeerasinghe <ravindi.20@cse.mrt.ac.lk>
2023-01-12 16:38:15 +0530

    Group 2 - Bank Transaction Management System

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-//import controllers
+//import routes
+const accRoutes = require("./app/routes/account.routes.js");
+const atmRoutes = require("./app/routes/atm.routes.js");
+const atmTranRoutes = require("./app/routes/atm_transaction.routes.js");
 const branchRoutes=require ("./app/routes/branch.routes.js");
+const branchManagerRoutes = require("./app/routes/branch_manager.routes.js");
+const childRoutes = require("./app/routes/child_customer.routes.js");
+const currAccRoutes = require("./app/routes/current_account.routes.js");
+const customerRoutes = require("./app/routes/customer.routes.js");
+const empRoutes = require("./app/routes/employee.routes.js");
+const fdIntRoutes = require("./app/routes/fd_interest.routes.js");
+const fdRoutes = require("./app/routes/fixed_deposit.routes.js");
+const indRoutes = require("./app/routes/individual_customer.routes.js");
+const loanRoutes = require("./app/routes/loan.routes.js");
+const loanAppRoutes = require("./app/routes/loan_application.routes.js");
 const installmentRoutes=require ("./app/routes/loan_installment.route.js");
 const lateInstallmentRoutes=require ("./app/routes/late_installment.route.js");
 const onlineApplicationRoutes=require ("./app/routes/online_loan_application.route.js");
@@ -20,15 +33,29 @@ app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
 //Routes
+app.use("/api/account",customerRoutes);
+app.use("/api/atm",atmRoutes);
+app.use("/api/atm_transaction",atmTranRoutes);
 app.use("/api/branch", branchRoutes);
-app.use("/api/installments",installmentRoutes);
+app.use("/api/branch_manager",branchManagerRoutes);
+app.use("/api/child_customer",childRoutes);
+app.use("/api/current_account",currAccRoutes);
+app.use("/api/customer",customerRoutes);
+app.use("/api/employee",empRoutes);
+app.use("/api/fd_interest",fdIntRoutes);
+app.use("/api/fixed_deposit",fdRoutes);
+app.use("/api/individual_customer",indRoutes);
+app.use("/api/loan",loanRoutes);
+app.use("/api/loan_application",loanAppRoutes);
+app.use("/api/loan_installments",installmentRoutes);
 app.use("/api/late_installments",lateInstallmentRoutes);
 app.use("/api/online_loan_application",onlineApplicationRoutes);
-app.use("/api/onlineTransaction",onlineTranRoutes);
+app.use("/api/online_transaction",onlineTranRoutes);
 app.use ("api/organization_customer",orgRoutes);
 app.use("/api/savings_account",savingsAccRoutes);
 app.use("/api/savings_acc_type",savAccTypeRoutes);
 app.use("/api/total_transaction",totTransactionRoutes);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
